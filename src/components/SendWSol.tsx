bf9a0eaf8ba5629d3cf50e60403ced622a892eb5
Baptiste Marchand <75846779+baptiste-marchand@users.noreply.github.com>
2025-04-22 23:08:11 +0200

    Revert "0.1.0 (#16)" (#20)
    This reverts commit 22ba2a15dc4c8c48f72c1f0c38bb72ea00dd3c07.

--- src/components/SendWSol.tsx
+++ src/components/SendWSol.tsx
@@ -223,7 +223,7 @@ export const SendWSol: FC = () => {
       const transactionHashs = [];
       for (const transaction of transactions) {
         const transactionHash = await sendTransaction(transaction, connection);
-        // await connection.confirmTransaction(transactionHash, 'confirmed'); // TODO: Disabled for E2E tests until local RPC is available
+        await connection.confirmTransaction(transactionHash, 'confirmed');
         transactionHashs.push(transactionHash);
       }
 
